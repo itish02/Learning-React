9f6b85054494c954afbdff21c97581349e6cfbe3
itish02 <78043075+itish02@users.noreply.github.com>
2021-04-14 15:31:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,39 @@ import Person from './Person/Person.js'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Itish', age: 23 },
-      { name: 'Mridul', age: 30 },
-      { name: 'Ishani', age: 29 },
-    ]
+      { id: 'asdf1', name: 'Itish', age: 23, job: 'Unemployed' },
+      { id: 'qwer2', name: 'Mridul', age: 30, job: 'Engineering Manager' },
+      { id:'zxcv3', name: 'Ishani', age: 29, job: 'Program Manager' },
+    ],
+    otherState: 'Some other value',
+    showPerson: false,
   }
 
-  switchNameHandler = (newName) => {
-    // DO NOT DO THIS - this.state.persons[0].name = 'Itish Dhiman';
-    this.setState({persons: [
-      { name: newName, age: 23 },
-      { name: 'Mridul', age: 30 },
-      { name: 'Ishani', age: 28 },
-    ]})
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const person = { 
+      ...this.state.persons[personIndex] 
+    };
+    person.name = event.target.value;
+
+    const persons = [ ...this.state.persons ];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+  
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      { name: 'Itish', age: 23 },
-      { name: event.target.value, age: 30 },
-      { name: 'Ishani', age: 28 },
-    ]})
-    // console.log(event);
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   render() {
@@ -38,27 +49,44 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+    let persons = null;
+    if(this.state.showPerson) {
+      persons = (
+      <div>
+        {this.state.persons.map((person, index) => {
+          return <Person 
+          click = {() => this.deletePersonHandler(index)}
+          name = {person.name}
+          age = {person.age}
+          job = {person.job}
+          key = {person.id} 
+          change = {(event) => this.nameChangedHandler(event, person.id)} />
+        })}
+        {/* <Person 
+           name = { this.state.persons[0].name } 
+           click = { this.switchNameHandler.bind(this, 'Itish Dhiman')}
+           age = { this.state.persons[0].age } job="unemployed"/>
+        <Person 
+           name = { this.state.persons[1].name } 
+           change = {this.nameChangedHandler}
+           age = { this.state.persons[1].age } job="Engineering Manager"/>
+        <Person 
+           name = { this.state.persons[2].name } 
+           age = { this.state.persons[2].age } job="Program Manager"/> */}
+      </div>
+      )
+    }
     return (
       // This looks like HTML, but it is NOT. This is JavaScript behind the scenes. 
+      
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <h2>This is very confusing so far 🤔 </h2>
        <p>Okay maybe i'm getting it...</p>
        <button 
        style={btnStyle}
-       onClick = { this.switchNameHandler.bind(this, 'Iti') }>Switch name</button>
-
-       <Person 
-       name = { this.state.persons[0].name } 
-       click = { this.switchNameHandler.bind(this, 'Itish Dhiman')}
-       age = { this.state.persons[0].age } job="unemployed"/>
-       <Person 
-       name = { this.state.persons[1].name } 
-       change = {this.nameChangedHandler}
-       age = { this.state.persons[1].age } job="Engineering Manager"/>
-       <Person 
-       name = { this.state.persons[2].name } 
-       age = { this.state.persons[2].age } job="Program Manager"/>
+       onClick = { this.togglePersonsHandler }>Toggle Names</button>
+       {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now? 😕 '));
@@ -67,6 +95,8 @@ class App extends Component {
 
 
 export default App;
+
+
 
 
 
