28fa68da05329194c3ab8cf54cc6aef09c514744
itish02 <78043075+itish02@users.noreply.github.com>
2021-04-15 22:32:07 +0530

    Update Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Person.css';
+
+const StyledDiv = styled.div`
+width: 60%;
+margin: 20px auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+`;
 
 const person = (props) => {
     return (
-        <div className="Person">
+        // < className="Person">
+        <StyledDiv>
             <p onClick = {props.click}>Hi, my name is {props.name} and I'm {props.age} years old. I am currently {props.job}.</p>
             <p>{props.children}</p>
             <input type = "text" onChange = {props.change} value = {props.name}/>
-        </div>
+        </StyledDiv>
     )
 }
 
