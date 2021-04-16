75e741823f8c328caaf3927f7cc0913f9c14cfd7
itish02 <78043075+itish02@users.noreply.github.com>
2021-04-16 16:25:35 +0530

    Started functional component based react

--- src/App.js
+++ src/App.js
@@ -1,150 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import Person from './Person/Person.js'
+import ExpenseItems from './components/ExpenseItem';
 
-const StyledButton = styled.button`
-      background-color: ${props => props.alt ? 'red' : 'green'};
-      color: white;
-      font: inherit;
-      border: 1px solid;
-      padding: 8px;
-      cursor: pointer;
+function App() {
+  const expenses = [
+    { title: 'Food 🍛', amount: 2000, date: new Date(2021, 4, 16) },
+    { title: 'Groceries 🥦', amount: 1200, date: new Date(2021, 4, 16) },
+    { title: 'Rent 🏠', amount: 10000, date: new Date(2021, 4, 16) },
+    { title: 'Netflix 🎥', amount: 800, date: new Date(2021, 5, 20)},
+  ];
+  return (
+    <div>
+      <h2> Let 's get started!</h2>
+      <p>Ah shit, here we go again..</p>
+      <ExpenseItems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}></ExpenseItems>
 
-      &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: black;
-      }
-`
+      <ExpenseItems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}></ExpenseItems>
 
-class App extends Component {
-  state = {
-    persons: [
-      { id: 'asdf1', name: 'Itish', age: 23, job: 'Unemployed' },
-      { id: 'qwer2', name: 'Mridul', age: 30, job: 'working at Noon Academy' },
-      { id:'zxcv3', name: 'Ishani', age: 29, job: 'working at Atlassian' },
-    ],
-    otherState: 'Some other value',
-    showPerson: false,
-  }
+      <ExpenseItems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}></ExpenseItems>
 
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    })
-
-    const person = { 
-      ...this.state.persons[personIndex] 
-    };
-    person.name = event.target.value;
-
-    const persons = [ ...this.state.persons ];
-    persons[personIndex] = person;
-
-    this.setState({persons: persons})
-  }
-  
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPerson;
-    this.setState({showPerson: !doesShow});
-  }
-
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
-
-  render() {
-    const btnStyle = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid',
-      padding: '8px',
-      cursor: 'pointer',
-    }
-    let persons = null;
-    if(this.state.showPerson) {
-      persons = (
-      <div>
-        {this.state.persons.map((person, index) => {
-          return <Person 
-          click = {() => this.deletePersonHandler(index)}
-          name = {person.name}
-          age = {person.age}
-          job = {person.job}
-          key = {person.id} 
-          change = {(event) => this.nameChangedHandler(event, person.id)} />
-        })}
-      </div>
-      );
-      btnStyle.backgroundColor = 'red';
-    }
-
-    const classes = [];
-    let displayText = "Okay maybe i'm getting it..."
-    if(this.state.persons.length <= 2) {
-      classes.push('red');
-      displayText = 'Hold up...'
-    }
-    if(this.state.persons.length <= 1) {
-      classes.push('bold');
-      displayText = 'Wtf did you do 😥';
-    }
-   
-    return (
-      // This looks like HTML, but it is NOT. This is JavaScript behind the scenes. 
-      
-      <div className="App">
-       <h1>Hi, I'm a React App</h1>
-       <h2>This is very confusing so far 🤔 </h2>
-       <p className = {classes.join(' ')}>{displayText}</p>
-       <StyledButton alt = {this.state.showPerson ? 1 : 0} onClick = { this.togglePersonsHandler }>Toggle Names</StyledButton> 
-       {persons}
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now? 😕 '));
-  }
+      <ExpenseItems
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}></ExpenseItems>
+    </div>
+  );
 }
-
 
 export default App;
 
-
-
-// const App = (props) => {
-//   const [ this,state. setthis state.] = useState({
-//       persons: [
-//         { name: 'Itish', age: 23 },
-//         { name: 'Mridul', age: 30 },
-//         { name: 'Ishani', age: 29 },
-//      ],
-//      otherState: 'some other value',
-//   });
-
-//   const switchNameHandler = () => {
-//         // DO NOT DO THIS - this.state.persons[0].name = 'Itish Dhiman';
-//         setthis(state.{persons: [
-//           { name: 'Itish Dhiman', age: 23 },
-//           { name: 'Mridul', age: 30 },
-//           { name: 'Ishani', age: 28 },
-//         ]
-//       });
-//     }
-//     const [someOtherState, setOtherState] = useState('some other value');
-//     console.log(this,state. someOtherState);
-//     return (
-//       // This looks like HTML, but it is NOT. This is JavaScript behind the scenes. 
-//       <div className="App">
-//        <h1>Hi, I'm a React App</h1>
-//        <h2>This is very confusing so far 🤔 </h2>
-//        <p>Okay maybe i'm getting it...</p>
-//        <button onClick = { switchNameHandler }>Switch name</button> 
-//        <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age } job="unemployed" />
-//        <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age } job="an Engineering Manager" />
-//        <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age } job="a Program Manager" />
-//       </div>
-//     );
-//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now? 😕 '));
-//   }
